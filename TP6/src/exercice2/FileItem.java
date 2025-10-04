1b37e1a2449bbf44987aa1690c48e68f6237ca0e
EttouyjerYasmine <ettouyjeryasmine@gmail.com>
2025-10-04 13:07:47 +0100

    Add files via upload

---
+++ TP6/src/exercice2/FileItem.java
@@ -0,0 +1,15 @@
+package exercice2;
+
+public class FileItem extends FsItem {
+    private long size; // en octets
+
+    public FileItem(String name, long size) {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+}
